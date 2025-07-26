939e2c452422da11b0c14cadec69753c1ad48304
abdelmalek-cpu <abdelmaleksmai60@gmail.com>
2025-07-26 03:16:08 +0100

    Added websockets

--- src/components/CreateGame.tsx
+++ src/components/CreateGame.tsx
@@ -1,22 +1,28 @@
 import { useNavigate } from 'react-router-dom';
 import type { Game } from '../../utilities/types';
-
+import useWebSocket from 'react-use-websocket';
 
 const CreateGame = () => {
   const navigate = useNavigate();
-
+  const { sendMessage } = useWebSocket('ws://localhost:4001');
+  
+  // we use websocket to communicate with the server instead of fetch 
 
   const handleCreate = async () => {
-    const response = await fetch('http://localhost:4000/create-game', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({})
-    });
-
-    const data: { game: Game } = await response.json();
-
-    navigate('/game', { state: { game: data.game , playerID: 1} });
+    sendMessage(JSON.stringify({ type: 'create-game' }));
   };
+  useWebSocket('ws://localhost:4001', {
+    onMessage: (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === 'game-created') {
+        const gameCode: string = data.payload.gameCode;
+        console.log(`Game created with code: ${gameCode}`);
+        navigate(`/game/${gameCode}`);
+      } else if (data.type === 'error') {
+        console.error(data.payload.message);
+      }
+    },
+  });
   
   return (
     <div>
