695ac94df93e29b9fce98e4eebc1d7534c8ab6b6
abdelmalek-cpu <abdelmaleksmai60@gmail.com>
2025-07-26 02:22:09 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import CreateGame from './components/CreateGame';
-import JoinGame from './components/JoinGame';
-import GamePlay from './components/GamePlay';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateGame from "./components/CreateGame";
+import JoinGame from "./components/JoinGame";
+import GamePlay from "./components/GamePlay";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <>
-            <h1>Prisoner's Dilema Game</h1>
-            <div className="game_control">
-              <CreateGame />
-              <JoinGame />
-            </div>
-            <p style={{color: 'grey'}}>If you have a game code press the JoinGame button.</p>
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Prisoner's Dilema Game</h1>
+              <div className="game_control">
+                <CreateGame />
+                <JoinGame />
+              </div>
+              <p style={{ color: "grey" }}>
+                If you have a game code press the JoinGame button.
+              </p>
+            </>
+          }
+        />
         <Route path="/game" element={<GamePlay />} />
       </Routes>
     </BrowserRouter>
