faa28d5a0ab34665df47d49b00ec7b667627b1d6
abdelmalek-cpu <abdelmaleksmai60@gmail.com>
2025-07-26 02:18:00 +0100

    making the code cute

---
+++ utilities/types.ts
@@ -0,0 +1,19 @@
+export type Choice = 'cooperate' | 'defect';
+
+export interface Player {
+    id : 1 | 2;
+    lastMove? : Choice
+    score : number;
+};
+
+export interface Game {
+    code: string;
+    players: Player[];
+    currentRound: number;
+    maxRounds: number;
+    history: {
+        p1: Choice;
+        p2: Choice;
+        result: [number, number];
+    }[]
+};
